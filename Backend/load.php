aa0679993f7cee3b80d91b76c8123d43781e0f90
Santhosh <sandysanthosh785@gmail.com>
2025-05-26 09:43:58 +0000

    login implementation by using post method

--- Backend/load.php
+++ Backend/load.php
@@ -4,3 +4,12 @@ function load_template($name)
 {
     include $_SERVER['DOCUMENT_ROOT'] . "/React_PHP/MilkMate/Backend/_templates/$name.php"; //not consistant.
 }
+
+function validate_credentials($username, $password)
+{
+    if ($username == "santhosh@gmail.com" and $password == "password") {
+        return true;
+    } else {
+        return false;
+    }
+}
