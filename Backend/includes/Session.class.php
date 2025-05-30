54c951fe8b1c475780ff6d9dc8a73e3a950ea6cd
Santhosh <sandysanthosh785@gmail.com>
2025-05-30 17:08:11 +0000

    Session implementation in class

--- Backend/includes/Session.class.php
+++ Backend/includes/Session.class.php
@@ -17,4 +17,23 @@ class Session
 
         session_destroy();
     }
+
+    public static function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public static function delete($key)
+    {
+        unset($_SERVER[$key]);
+    }
+    public static function isset($key)
+    {
+        return isset($_SESSION[$key]);
+    }
+
+    public static function get($key)
+    {
+        return $_SESSION($key);
+    }
 }
