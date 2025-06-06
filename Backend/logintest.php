80e757840ec2b415e24156211bfa5fc46db4cb87
Santhosh <sandysanthosh785@gmail.com>
2025-06-06 10:21:58 +0000

    Login test

--- Backend/logintest.php
+++ Backend/logintest.php
@@ -5,34 +5,27 @@ $user = "santhosh";
 $pass = isset($_GET['pass']) ? $_GET['pass'] : '';
 $result = null;
 
-if (isset($_GET['logout'])) {
-    Session::Destroy();
-    die("Session destroyed,<a href='logintest.php'>LoginAgain</a>");
-}
-
 if (Session::get('is_loggedin')) {
-    $userdata = Session::get('session_user');
-    print("Welcome Back,$userdata[username]");
-    $result = $userdata;
+    $username = Session::get('session_username');
+    $userobj = new User($username);
+    print("Welcome Back " . $userobj->getFirstname());
+    print("<br>" . $userobj->getBio());
+    $userobj->setBio("Making new things...");
+    print("<br>" . $userobj->getBio());
 } else {
-    printf("No Session found, Trying to login now .<br>");
+    printf("No session found, trying to login now. <br>");
     $result = User::validate_credentials($user, $pass);
+
     if ($result) {
-        session_regenerate_id(true);
-        echo "Login Success,$result[username]";
+        $userobj = new User($user);
+        echo "Login Success ", $userobj->getUsername();
         Session::set('is_loggedin', true);
-        Session::set('session_user', $result);
+        Session::set('session_username', $result);
     } else {
-        echo "Login Failed, $user <br>";
+        echo "Login failed, $user <br>";
     }
 }
+
 echo <<<EOL
 <br><br><a href="logintest.php?logout">Logout</a>
 EOL;
-
-// $result = User::validate_credentials($user, $pass);
-// if ($result) {
-//     echo ("Login Sucesss");
-// } else {
-//     echo ("Login Fail");
-// }
